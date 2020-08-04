24396440a1f3e07ed59744a999c044d7b97ba582
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-08-04 08:11:13 -0700

    Updated expert.sh

--- expert.sh
+++ expert.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=2 python3 main.py --id planet_expert_offline --env cartpole-balance --action-repeat 8 \
- --experience-replay /data/ashwin/planet/balance_mse10_expert_experience.pth \
---offline --episodes 3000
+python3 main.py --id planet_offline_swingup_noise0.5 --env cartpole-swingup --action-repeat 8 \
+ --experience-replay /data/ashwin/datasets/swingup_noisy_expert0.5_experience/swingup_noisy_expert0.5_experience.pth \
+--offline --episodes 1000 --gpu-id 2
